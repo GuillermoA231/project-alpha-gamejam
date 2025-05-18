2609d7cf58828c3c4892b589980d8f72a747df86
GuillermoA231 <AndradaJoaquin302@gmail.com>
2025-05-18 10:47:56 -0300

    added more assets

--- Assets/Scripts/EnemyMovementy.cs
+++ Assets/Scripts/EnemyMovementy.cs
@@ -7,6 +7,11 @@ public class EnemyMovementy : MonoBehaviour
     private Player player;
     [Header(" Settings")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float playerDetectionRadius;
+
+
+    [Header("DEBUG")]
+    [SerializeField] private bool showGizmos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,10 +27,37 @@ public class EnemyMovementy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FollowPlayer();
+
+        TryAttack();
+    }
+
+    private void FollowPlayer()
+    {
         Vector2 direction = (player.transform.position - transform.position).normalized;
 
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
+
+    }
+
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= playerDetectionRadius)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!showGizmos)
+        { return; } 
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,playerDetectionRadius);
     }
 }
