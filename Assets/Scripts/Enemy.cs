8f731f7c0ab94e1aae94bc61e5ce4d2f2dea74f3
GuillermoA231 <AndradaJoaquin302@gmail.com>
2025-05-19 10:29:16 -0300

    Refactored enemy movement, spawning and dead particles into Enemy.cs - Fixed Typo (EnemyMovementy -> EnemyMovement)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,13 +2,133 @@
 using UnityEngine;
 using System;
 
+
+[RequireComponent(typeof(EnemyMovement))]
 public class Enemy : MonoBehaviour
 {
+
+    [Header("Components")]
+    private EnemyMovement movement;
+
+    [Header(" Elements")]
+    private Player player;
+
+
+    [Header("Spawn Sequence")]
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+    [SerializeField] private float spawnScaling, spawnScalingTime;
+    private bool hasSpawned;
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem deathParticles;
+
+
+    [Header("Attack")]
+    [SerializeField] private int damage;
+    [SerializeField] private float attackFrequency;
+    [SerializeField] private float playerDetectionRadius;
+    private float attackDelay, attackTimer;
+
+
+    [Header("DEBUG")]
+    [SerializeField] private bool showGizmos;
+
     public event Action<GameObject> OnEnemyDied;
+
+    void Start()
+    {
+
+        movement = GetComponent<EnemyMovement>();
+
+        player = FindFirstObjectByType<Player>();
+
+        if (player == null)
+        {
+            Debug.LogWarning("No player found, Destroying");
+            Destroy(gameObject);
+        }
+        StartSpawnSequence();
+
+        attackDelay = 1f / attackFrequency;
+    }
+
+    void Update()
+    {
+        if (attackTimer >= attackDelay)
+            TryAttack();
+        else
+            Wait();
+    }
+
+    private void StartSpawnSequence()
+    {
+        SetRenderersVisible(false);
+        Vector3 targetScale = spawnIndicator.transform.localScale * spawnScaling;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, spawnScalingTime)
+                 .setLoopPingPong(3)
+                 .setOnComplete(SpawnSequenceCompleted);
+
+
+    }
+
+
+    private void SpawnSequenceCompleted()
+    {
+        SetRenderersVisible(true);
+        hasSpawned = true;
+
+        movement.StorePlayer(player);
+
+    }
+
+
+    private void SetRenderersVisible(bool visibility)
+    {
+        renderer.enabled = visibility;
+        spawnIndicator.enabled = !visibility;
+        
+    }
+    private void TryAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= playerDetectionRadius)
+            Attack();
+
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Damage done: " + damage);
+        attackTimer = 0;
+    }
+
+    private void Wait()
+    {
+        attackTimer += Time.deltaTime;
+    }
+
+
+    private void OnDeath()
+    {
+        deathParticles.transform.SetParent(null);
+        deathParticles.Play();
+        Destroy(gameObject);
+
+    }
 
     public void Die()
     {
         OnEnemyDied?.Invoke(this.gameObject);
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!showGizmos)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header(" Elements")]
+    private Player player;
+
+    [Header(" Settings")]
+    [SerializeField] private float moveSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+            FollowPlayer();
+    }
+
+    public void StorePlayer(Player player)
+    {
+        this.player = player; 
+    }
+
+
+    private void FollowPlayer()
+    {
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+
+        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
+
+        transform.position = targetPosition;
+
+    }
+
+
+
+}
