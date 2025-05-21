b0114800595a0a8878c4c120553040c7105f8c44
GuillermoA231 <AndradaJoaquin302@gmail.com>
2025-05-21 20:31:16 -0300

    Added weapon attack

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -22,10 +23,13 @@ public class Weapon : MonoBehaviour
     [SerializeField] private LayerMask enemyMask;
     [Header("Attack")]
     [SerializeField] private int damage;
+    [SerializeField] private float attackDelay;
+    [SerializeField] private Animator animator;
+    private float attackTimer;
+    private List<Enemy> damagedEnemies = new List<Enemy>();
 
     [Header("Animations")]
     [SerializeField] private float aimLerp;
-    [SerializeField] private Animator animator;
 
     [Header("DEBUG")]
     [SerializeField] private bool showGizmos;
@@ -50,7 +54,7 @@ public class Weapon : MonoBehaviour
                 Attacking();
                 break;
         }
-    } 
+    }
 
 
     private void AutoAim()
@@ -59,9 +63,26 @@ public class Weapon : MonoBehaviour
         Vector2 targetUpVector = Vector3.up;
 
         if (closestEnemy != null)
+        {
+            ManageAttack();
             targetUpVector = (closestEnemy.transform.position - transform.position).normalized;
+        }
 
         transform.up = Vector3.Lerp(transform.up, targetUpVector, Time.deltaTime * aimLerp);
+        IncrementAttackTimer();
+    }
+    private void ManageAttack()
+    {
+        if (attackTimer >= attackDelay)
+        {
+            attackTimer = 0;
+            StartAttack();
+        }
+    }
+
+    private void IncrementAttackTimer()
+    {
+        attackTimer += Time.deltaTime;
     }
 
 
@@ -70,6 +91,8 @@ public class Weapon : MonoBehaviour
     {
         animator.Play("Attack");
         state = State.Attack;
+        
+        damagedEnemies.Clear();
     }
 
     private void Attacking()
@@ -80,6 +103,10 @@ public class Weapon : MonoBehaviour
     private void StopAttack()
     {
         state = State.Idle;
+
+        //Clear the attacked enemies
+        // Damaged enemies list
+        damagedEnemies.Clear();
     }
 
     private void Attack()
@@ -88,7 +115,18 @@ public class Weapon : MonoBehaviour
 
         for (int i = 0; i < enemies.Length; i++)
         {
-            enemies[i].GetComponent<Enemy>().TakeDamage(damage);
+            Enemy enemy = enemies[i].GetComponent<Enemy>();
+            // 1. Is the enemy inside of the list ?
+            if (!damagedEnemies.Contains(enemy))
+            {
+                enemy.TakeDamage(damage);
+                damagedEnemies.Add(enemy);
+            }
+
+            // 2.  If no, let's attack him, and add it to the list
+
+            // 3 . If yes, it's inside of the list, let's continue
+
         }
 
     }
