32526d3ed1650b0bc7ef2cd3cd8fc15c1588cabd
GuillermoA231 <AndradaJoaquin302@gmail.com>
2025-05-31 16:07:05 -0300

    Wave Manager +  Wave Transition

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -8,3 +8,43 @@ public enum GameState
     SHOP,
     GAMEOVER
 }
+
+public enum Stat
+{
+    Attack,
+    AttackSpeed,
+    CriticalChance,
+    CriticalDamage,
+    MoveSpeed,
+    MaxHealth,
+    Range,
+    HealthRecoverySpeed,
+    Armor,
+    Luck,
+    Dodge,
+    Lifesteal,
+}
+
+public static class Enums
+{
+    public static string FormatStatName(Stat stat)
+    {
+        string formated = "";
+        string unformattedString = stat.ToString();
+
+        if (unformattedString.Length <= 0)
+            return "Unvalid Stat name";
+
+        formated += unformattedString[0];
+
+        for (int i = 1; i < unformattedString.Length; i++)
+        {
+            if (char.IsUpper(unformattedString[i]))
+                formated += " ";
+
+            formated += unformattedString[i];
+        }
+
+        return formated;
+    }
+}
