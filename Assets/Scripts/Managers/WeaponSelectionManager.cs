c60611451288d5bb675bbc18a81a5a56af852b9f
GuillermoA231 <AndradaJoaquin302@gmail.com>
2025-06-04 06:33:46 -0300

    added weapon prefab and basic weapon manager logic

--- Assets/Scripts/Managers/WeaponSelectionManager.cs
+++ Assets/Scripts/Managers/WeaponSelectionManager.cs
@@ -1,16 +1,56 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class WeaponSelectionManager : MonoBehaviour
+public class WeaponSelectionManager : MonoBehaviour, IGameStateListener
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header(" Elements")]
+    [SerializeField] private Transform containersParent;
+    [SerializeField] private WeaponSelectionContainer weaponContainerPrefab;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    public void GameStateChangedCallback(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.WEAPONSELECTION:
+                Configure();
+                break;
+
+        }
+    }
+
+    private void Configure()
+    {
+        CleanContainerChildren();
+
+        for (int i = 0; i < 3; i++)
+        {
+            GenerateWeaponContainer();
+        }
+
+    }
+
+    private void CleanContainerChildren()
+    {
+        while (transform.childCount > 0)
+        {
+            Transform child = transform.transform.GetChild(0);
+            child.SetParent(null);
+            Object.Destroy(child.gameObject);
+        }
+    }
+
+    private void GenerateWeaponContainer()
+    {
+        WeaponSelectionContainer containerInstance = Instantiate(weaponContainerPrefab, containersParent);
+    }
+
 }
